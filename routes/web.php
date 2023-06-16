1cfc7d4b0fec5f488df162fc183feee86a9e7edc
Erick Zambrano <erickz16@hotmail.com>
2023-06-15 23:24:19 -0500

    se crea el crud para autores, libros, editoriales, ejemplares y se agrega el login de la aplicacion

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AutorController;
+use App\Http\Controllers\LibroController;
+use App\Http\Controllers\EditorialController;
+use App\Http\Controllers\EjemplarController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +20,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+Route::resource('dashboard/libro', LibroController::class);
+Route::resource('dashboard/autor', AutorController::class);
+Route::resource('dashboard/editorial', EditorialController::class);
+Route::resource('dashboard/ejemplar', EjemplarController::class);
